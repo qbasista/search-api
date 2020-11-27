199581f6e03c8d079bc9cde95c7cf08c14705ec4
Kuba Wojciechowski <catchthemlive@gmail.com>
2020-11-27 22:16:20 +0100

    added query and item model

--- search_api/search_engine/models.py
+++ search_api/search_engine/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Query(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+    total_result = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Item(models.Model):
+    position = models.IntegerField(blank=False)
+    link = models.CharField(max_length=256)
+    title = models.CharField(max_length=256)
+    desc = models.TextField()
+    query = models.ForeignKey(
+        Query, on_delete=models.CASCADE, related_name='items')
---
+++ search_api/search_engine/tests/test_models.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from search_engine.models import Query, Item
+
+DESC = """Lorem Ipsum is simply dummy 
+            text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard 
+            dummy text ever since the 1500s, when an unknown 
+            printer took a galley of type and scrambled it to make a 
+            type specimen book. It has survived not only five centuries, 
+            but also the leap into electronic typesetting, remaining essentially unchanged."""
+
+
+class QueryTest(TestCase):
+
+    def test_create_query(self):
+        name = 'book'
+        total_result = 1000
+
+        q = Query(
+            name=name,
+            total_result=total_result,
+        )
+        q.save()
+        item1 = q.items.create(
+            position=1,
+            link='https://www.some-link.com',
+            title='some title',
+            desc=DESC
+        )
+        item2 = q.items.create(
+            position=2,
+            link='https://some-link2.com',
+            title='some title 2',
+            desc=DESC
+        )
+
+        self.assertIsInstance(q, Query)
+        self.assertIsInstance(item1, Item)
+        self.assertIsInstance(item2, Item)
+        self.assertEqual(q.name, name)
+        self.assertEqual(q.total_result, total_result)
+        self.assertEqual(len(q.items.all()), 2)
